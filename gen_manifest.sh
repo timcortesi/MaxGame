506695ec5426d756d5d7b200ff5a9d40a716686e
Tim Cortesi <tcortesi@gmail.com>
2019-07-20 15:12:10 -0400

    Replace Old Style Manifests

--- gen_manifest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "CACHE MAINFEST"
-echo
-find * -not -path "*/\.*" -not -path "*.manifest" -not -path "*.sh" -not -path "index.html" -not -type d
---
+++ manifest_gen.php
@@ -0,0 +1,19 @@
+<?php
+
+$manifest = [
+    'short_name' => "Max's Game",
+    'name' => "Max's Name Game",
+    "icons"=> [
+        [
+            "src"=> "assets/images/max-192.png",
+            "type"=> "image/png",
+            "sizes"=> "192x192"
+        ],
+    ],
+    "start_url"=> "index.html",
+    "background_color"=> "#fff",
+    "display"=> "fullscreen",
+    "orientation"=> "landscape",
+    "scope"=> "/",
+];
+file_put_contents('manifest.json',json_encode($manifest));
